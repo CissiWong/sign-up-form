0a15c338e95921588d86a085457217bfa24926fe
Damien <mail@damientimewell.com>
2018-01-05 13:51:05 +0100

    First draft of the assignment

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose"
+import express from "express"
+import bodyParser from "body-parser"
+import bcrypt from "bcrypt-nodejs"
+
+// Express setup, including JSON body parsing.
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// Connect to MongoDB, on the "products-api" database. If the db doesn't exist, mongo will create it.
+mongoose.connect("mongodb://localhost/signup-form-api", { useMongoClient: true })
+
+// This makes mongo use ES6 promises, instead of its own implementation
+mongoose.Promise = Promise
+
+// Log when mongo connects, or encounters errors when trying to connect.
+mongoose.connection.on("error", err => console.error("Connection error:", err))
+mongoose.connection.once("open", () => console.log("Connected to mongodb"))
+
+//
+// Define a model here.
+//
+
+// Example root endpoint to get started with
+app.get("/", (req, res) => {
+  const password = "supersecretpassword"
+  const hash = bcrypt.hashSync(password)
+
+  // bcrypt.compareSync("supersecretpassword", hash) // true
+  // bcrypt.compareSync("incorrectpassword", hash) // false
+
+  res.send(`Signup form api. Here's an example of an encrypted password: ${hash}`)
+})
+
+// Add more endpoints here!
+
+app.listen(8080, () => console.log("Products API listening on port 8080!"))
